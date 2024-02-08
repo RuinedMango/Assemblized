ae9e5b9d12260ed12ee1f1d4169c5ae3aac88053
Ruinedmango <00rudystx@gmail.com>
2024-02-07 15:32:23 -0900

    Create WindowsTest.asm

---
+++ WindowsTest.asm
@@ -0,0 +1,23 @@
+global start
+
+NULL  equ 0
+MB_OK equ 0
+
+extern MessageBoxA   ;from user32
+extern ExitProcess   ;from kernel32
+
+section .data
+hello:  db 'Fuck You!',0
+title:  db 'My First Win32',0
+
+section .text
+start:
+
+        push    MB_OK
+        push    title
+        push    hello
+        push    NULL
+        call    MessageBoxA
+
+        push    0
+        call    ExitProcess
